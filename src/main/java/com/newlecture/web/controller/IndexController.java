91fc7a2be118aa8e3306a243bc6d4ef2f9862a65
HongDongHwan <hongdh94@gmail.com>
2022-07-12 20:11:31 +0900

    ADD Spring MVC
    0712 Spring MVC Study
    
    8 - ViewResolver 사용하기
    View페이지 쉽게 찾는 방법
    9 - HTML 파일 설정하기
    10 - 정적 파일 서비스하기
    11 - 공지사항 컨트롤러 추가하기
    12 - Detail 컨트롤러 추가와 View페이지 집중화의 필요성
    13 - 페이지 공통분모 집중화
    14 - 페이지 모듈 분리하기

--- src/main/java/com/newlecture/web/controller/IndexController.java
+++ src/main/java/com/newlecture/web/controller/IndexController.java
@@ -5,18 +5,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 public class IndexController implements Controller{
 
 	
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		System.out.println("이게 나와야해...");
-		System.out.println("master branch test");
-		System.out.println("branch Test");
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("data", "hello Spring MVC~");
-		mv.setViewName("/WEB-INF/view/index.jsp");
 		
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("data", "hello Spring MVC~");
+		// mv.setViewName("/WEB-INF/view/index.jsp");
+
 		return mv;
 	}
 	
